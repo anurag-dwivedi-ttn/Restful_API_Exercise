f4cef821f249df017f609bc8699e7b99cfeb484e
anurag-dwivedi-ttn <anurag.dwivedi@tothenew.com>
2025-03-08 18:53:44 +0530

    SayHelloController Class Added

--- src/main/java/com/example/controllerdemo/controller/SayHelloController.java
+++ src/main/java/com/example/controllerdemo/controller/SayHelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MyController {
+public class SayHelloController {
 
 //    // without response body
 //    @GetMapping("/without-body")
